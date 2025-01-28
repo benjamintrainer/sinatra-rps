907eb9407378257e09d13f17fe8bc45627868058
benjamintrainer <155780688+benjamintrainer@users.noreply.github.com>
2025-01-28 17:39:35 +0000

    RPS v1

--- app.rb
+++ app.rb
@@ -2,8 +2,50 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:home)
+end
+
+get("/rock") do
+  moves = ["rock", "paper", "scissors"]
+
+  @computer_move = moves.sample
+
+  if @computer_move == "rock"
+    @outcome = "tied"
+  elsif @computer_move == "scissors"
+    @outcome = "won"
+  else
+    @outcome = "lost"
+  end
+  erb(:rock)
+end
+
+get("/paper") do
+  moves = ["rock", "paper", "scissors"]
+
+  @computer_move = moves.sample
+
+  if @computer_move == "paper"
+    @outcome = "tied"
+  elsif @computer_move == "rock"
+    @outcome = "won"
+  else
+    @outcome = "lost"
+  end
+  erb(:paper)
+end
+
+get("/scissors") do
+  moves = ["rock", "paper", "scissors"]
+
+  @computer_move = moves.sample
+
+  if @computer_move == "scissors"
+    @outcome = "tied"
+  elsif @computer_move == "paper"
+    @outcome = "won"
+  else
+    @outcome = "lost"
+  end
+  erb(:scissors)
 end
